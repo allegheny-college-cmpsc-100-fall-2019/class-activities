a73a93e558436af8ca6a4358c088e707d2173b87
Douglas Luman <dluman@douglasjluman.com>
2019-09-08 19:57:13 -0400

    Adding companion file for activity.

---
+++ 9 September/GreetWorld.java
@@ -0,0 +1,7 @@
+import greetings.HelloWorld;
+
+public class GreetWorld {
+  public static void main (String[] args) {
+    HelloWorld.main(null);
+  }
+}
--- 9 September/HelloWorld.java
+++ 9 September/HelloWorld.java
@@ -1,3 +1,5 @@
+package greetings;
+
 public class HelloWorld {
   public static void main(String[] args) {
     // This is a comment. Does it display?
