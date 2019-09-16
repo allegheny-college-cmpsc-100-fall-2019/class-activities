a94b486bcfca1570f630748f69c68f2fd9edb945
Douglas Luman <dluman@douglasjluman.com>
2019-09-16 10:34:53 -0400

    Adding end of classs activity for 16 September

---
+++ 16-september/src/main/java/ThievesStory/ThievesStory.java
@@ -0,0 +1,23 @@
+package wordproblems;
+
+/** This class solves the Thieves' Story.
+ *
+ * @author Douglas Luman
+ */
+public class ThievesStory {
+   
+  final static int jewels;
+  final static int thieves;
+  final static int minShare;
+  
+  /** This is the entry point.
+   *
+   * @param The command line arguments.
+   */
+  public static void main(String[] args) {
+    int seniorMembers;
+    int juniorMembers;
+    System.out.print("There are " + juniorMembers + " junior members, " +
+                     "and " + seniorMembers + " senior members.");
+  }
+}
