c40e9d4007e083be43cca222c8c82f6c0e6b8305
Douglas Luman <dluman@douglasjluman.com>
2019-09-25 10:07:11 -0400

    Adding class activity shell for the day.

---
+++ 25-september/src/main/java/methodtest/MethodTest.java
@@ -0,0 +1,35 @@
+package experiments;
+
+import java.util.Scanner;
+
+/** Expermenting with Java functions!
+ *
+ * @author Douglas Luman
+ */
+public class MethodTest {
+  
+  /** This is the main entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args){
+    // Set up the scanner for user input
+    Scanner scan = new Scanner(System.in);
+    /*
+     * TODO Choose between functions from the Math or String classes
+     * and implement at least three methods of your choice! This will
+     * affect your implementation of the Scanner; will it  be
+     * nextInt(), nextDouble(), nextLine()?
+     */
+  }
+  
+  /** Test method for the bold; uncomment to use.
+   *
+   * @param YOU decide!
+  */
+  /*
+  public {YOUR CHOICE} testMethod({YOUR CHOICE}){
+  
+  }
+  */
+}
