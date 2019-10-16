009416baacdc194b1bd935144b6f6ad62288e0b5
Douglas Luman <dluman@douglasjluman.com>
2019-10-16 10:03:23 -0400

    Adding activity for 16-october.

---
+++ 16-october/src/main/java/wordproblems/PrisonersDilemma.java
@@ -0,0 +1,27 @@
+package wordproblems;
+
+import java.util.Scanner;
+
+public class PrisonersDilemma {
+  public static void main(String[] args){
+    // Initialize objects
+    String decision = new String();
+    Scanner input = new Scanner(System.in);
+    // Print and receive input at prompt
+    System.out.print("Does Alice defect [T/F]: ");
+    decision = input.nextLine().toUpperCase();
+    // Set up boolean object
+    boolean defectA = decision.equals("T");
+    System.out.print("Does Bob defect [T/F]: ");
+    decision = input.nextLine().toUpperCase();
+    // Set up boolean object
+    boolean defectB = decision.equals("T");
+    // Initialize point values
+    int yearsA = 0;
+    int yearsB = 0;
+    // Implement the logic
+    // Print results
+    System.out.println("\nAlice's sentence: " + yearsA + " year(s).");
+    System.out.println("\nBob's sentence: " + yearsB + " year(s).");
+  }
+}
