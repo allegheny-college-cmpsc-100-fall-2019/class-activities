ff039439c56f95e92e724acb4cce0b0ab609fdac
Douglas Luman <dluman@douglasjluman.com>
2019-10-23 10:49:21 -0400

    Adding activity.

--- 23-october/ArrayList/src/main/java/library/DisplayBooks.java
+++ 23-october/ArrayList/src/main/java/library/DisplayBooks.java
@@ -3,16 +3,8 @@ package arraylist;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/** Accepts entries and displays books.
- *
- * @author {YOUR NAME HERE}
- */
 public class DisplayBooks {
   
-  /** Entry point.
-   *
-   * @param args The command line arguments.
-   */
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
     
